0ec54af174bb7817cc58376356c49cab6b289740
Elrey Bellmonti and Kelsey Langlois <kelsey@>
2017-08-14 09:36:44 -0700

    Refactor code to use loop for event listeners.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,12 @@
 $(document).ready(function(){
-  // $("h1").click(function() {
-  //   alert("This is a header.");
-  //   alert("I told you, THIS IS A HEADER!");
-  // });
 
-  // $("p").click(function() {
-  //   alert("This is a paragraph.");
-  // });
+  var tags = ["h1", "p", "img"];
+  tags.forEach(function(tag) {
+    $(tag).click(function() {
+      alert("This is a " + tag);
+    });
+  });
 
-  // $("img").click(function() {
-  //   alert("This is an image.");
-  // });
   $(".clickable").click(function(){
     $(".initially-hidden").toggle();
     $(".initially-showing").toggle();
